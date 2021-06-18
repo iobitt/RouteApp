b4d699acc72e33279a48ca181c0ca431312fafeb
iobitt <sashabratm@gmail.com>
2021-06-18 17:47:38 +0300

    code review

--- home/views.py
+++ home/views.py
@@ -5,5 +5,7 @@ from route.models import *
 
 @login_required
 def index(request):
+    """ Возвращает главную страницу """
+
     routes = Route.objects.filter(owner_id=request.user.id)
     return render(request, "home/index.html", {"routes": routes})
