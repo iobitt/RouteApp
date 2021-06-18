61609fb26a80dac87f8670bd2c8105b638604c6e
iobitt <sashabratm@gmail.com>
2021-06-18 17:47:21 +0300

    add logout, code review

--- account/forms.py
+++ account/forms.py
@@ -3,20 +3,30 @@ from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
+    """ Форма авторизация """
+
+    # Имя пользователя
     username = forms.CharField()
+    # Пароль
     password = forms.CharField(widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(forms.ModelForm):
+    """ Форма регистрации """
+
+    # Пароль
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    # Повтор пароля
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
 
-    def clean_password2(self):
+    def clean_password2(self) -> str:
+        """ Сравнивает пароль и повтор пароля. Если пароли не совпадают, бросает исключение. Иначе возвращает пароль """
+
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
+            raise forms.ValidationError('Пароли не совпадают!')
         return cd['password2']
--- account/views.py
+++ account/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
+    """ Авторизовать пользователя """
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -14,27 +17,34 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return HttpResponse('Авторизация прошла успешно!')
                 else:
-                    return HttpResponse('Disabled account')
+                    return HttpResponse('Аккаунт деактивирован!')
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse('Не верный логин!')
     else:
         form = LoginForm()
     return render(request, 'registration/login.html', {'form': form})
 
 
 def register(request):
+    """ Зарегистрировать пользователя """
+
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
             return redirect('/')
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {'user_form': user_form})
+    return render(request, 'registration/register.html', {'form': user_form})
+
+
+@login_required
+def logout_view(request):
+    """ Деавторизовать пользователя """
+
+    logout(request)
+    return redirect('/accounts/login/')
